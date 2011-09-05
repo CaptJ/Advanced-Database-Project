fc5c87f97fac1bc490f2947be6e35735aee7e789
Lem Lordje Ko <lord.lem.l@gmail.com>
2011-09-06 00:50:31 +0800

    Skeleton

---
+++ lem.rb
@@ -0,0 +1,41 @@
+require 'sinatra'
+require 'pg'
+require 'json'
+
+enable :sessions
+
+conn = PG.connect(
+    :host => "localhost",   :port => 5432,      :dbname => "DSConnect",
+    :user => "lem",         :password = "s"
+    )
+
+class PGresult
+    def to_h(key_field='id')
+        the_h = Hash.new
+
+        self.each do |row|
+            the_h[row[key_field]] = row
+        end
+
+        the_h
+     end
+
+     def to_a
+        the_a = Array.new
+
+        self.each do |row|
+            the_a << row
+        end
+
+        the_a
+     end
+
+     def to_json(*a)
+        self.to_a.to_json(*a)
+     end
+end
+
+
+get '/' do 
+    "Hello World" 
+end
